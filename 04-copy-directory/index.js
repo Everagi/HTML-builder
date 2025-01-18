4912a0dd70b4939790a0773147228956d4796223
Everagi <belorusskiychuvak@gmail.com>
2025-01-18 21:36:25 +0300

    docs: create a function template

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const sourceDir = path.join(__dirname, 'files');
+const targetDir = path.join(__dirname, 'files-copy');
+
+
+async function copyDir(){
+    try {
+        await fs.mkdir(targetDir, { recursive: true });
+        const files = await fs.readdir(sourceDir, { withFileTypes: true });
+    }catch (error) {
+        console.error('Error copying:', error);
+      }
+}
