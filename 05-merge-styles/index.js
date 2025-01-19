34021ca4ce9ac648d4a5b6e4eb9dc6f4ad8aa788
Everagi <belorusskiychuvak@gmail.com>
2025-01-19 18:59:03 +0300

    docs: create fn filter css files

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,22 +6,42 @@ const distDir = path.join(__dirname, 'project-dist');
 const outputFile = path.join(distDir, 'bundle.css'); 
 
 async function readStylesDir() {
-    try {
-        const files = await fs.readdir(stylesDir, { withFileTypes: true });
-        return files;
-    }catch (error) {
-        console.error('Error reading folder styles:', error);
-        throw error; // Пробрасываем ошибку
-    }   
+  try {
+    const files = await fs.readdir(stylesDir, { withFileTypes: true });
+    return files;
+  } catch (error) {
+      console.error('Error reading folder styles:', error);
+      throw error;
+  }   
 }
 
-async function checkup() {
-    try {
-      const files = await readStylesDir();
-      console.log('Содержимое папки styles:', files);
-    } catch (error) {
-      console.error('Ошибка процессе:', error);
-    }
+function isCssFile(file) {
+  return file.isFile() && path.extname(file.name) === '.css';  // Проверка расширения
+}
+
+function mergeStyles(styles) {
+  console.log('merge')
+}
+
+function writeBundle(content) {
+  console.log('writw')
+}
+
+async function buildBundle() {
+  try {
+    const stylesArray = [];
+    const files = await readStylesDir();
+    const cssFiles = files.filter((file) => isCssFile(file));
+    return cssFiles;
+  } catch (error) {
+    console.error('Error creating bundle.css: ', error);
   }
-  
-  checkup();
+}
+
+
+async function testBuildBundle() {
+  const arr = await buildBundle();
+  console.log(arr);  
+}
+
+testBuildBundle();
